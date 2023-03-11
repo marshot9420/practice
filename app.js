addb17c00b8178a6d219a5c8d254b012f15b2d67
marshot <124557537+marshot9420@users.noreply.github.com>
2023-03-11 20:50:16 +0900

    Responsive Web Design

--- app.js
+++ app.js
@@ -14,8 +14,21 @@ const darkBtn = document.getElementById("dark-btn");
 
 const gift = document.getElementById("gift");
 
+const slideContainer = document.querySelector(".slide-container");
+const prevBtn = document.querySelector(".prevBtn");
+const slideBtn1 = document.querySelector(".slide-1");
+const slideBtn2 = document.querySelector(".slide-2");
+const slideBtn3 = document.querySelector(".slide-3");
+const nextBtn = document.querySelector(".nextBtn");
+
+const navbarBrand = document.getElementById("navbar-text");
+
+const lorem = document.querySelector(".lorem");
+
 let darkBtnCount = 0;
 let giftCount = 5;
+let transValue = 0;
+let fontValue = 30;
 
 const handleGift = () => {
   gift.style.display = "none";
@@ -49,6 +62,8 @@ const handleLoginAlert = (e) => {
   } else if (pw.value.length < 8) {
     e.preventDefault();
     alert("비밀번호를 8자 이상 입력해 주세요!");
+  } else if (/\S+@\S+.\S+/.test(email.value)) {
+    alert("이메일 형식으로 입력해 주세요.");
   }
 };
 
@@ -82,6 +97,38 @@ const handleModalClose = () => {
   modal.style.display = "none";
 };
 
+const handleSlider1 = () => {
+  slideContainer.style.transform = "translateX(0vw)";
+};
+
+const handleSlider2 = () => {
+  slideContainer.style.transform = `translateX(-100vw)`;
+};
+
+const handleSlider3 = () => {
+  slideContainer.style.transform = `translateX(-200vw)`;
+};
+
+const handleSlidePrev = () => {
+  transValue = transValue + 100;
+  slideContainer.style.transform = `translateX(${transValue}vw)`;
+};
+
+const handleSlideNext = () => {
+  transValue = transValue - 100;
+  slideContainer.style.transform = `translateX(${transValue}vw)`;
+};
+
+const handleNavbarFontDown = () => {
+  fontValue = fontValue - 1;
+  navbarBrand.style.fontSize = `${fontValue}px`;
+};
+
+const handleNavbarFontUp = () => {
+  fontValue = fontValue + 1;
+  navbarBrand.style.fontSize = `${fontValue}px`;
+};
+
 toggler.addEventListener("click", handleListGroup);
 openBtn.addEventListener("click", handleModalOpen);
 closeBtn.addEventListener("click", handleModalClose);
@@ -89,3 +136,12 @@ submitBtn.addEventListener("click", handleLoginAlert);
 pw.addEventListener("input", handlePwInput);
 darkBtn.addEventListener("click", handleDarkMode);
 setInterval(handleGiftCount, 1000);
+slideBtn1.addEventListener("click", handleSlider1);
+slideBtn2.addEventListener("click", handleSlider2);
+slideBtn3.addEventListener("click", handleSlider3);
+if (transValue < 0) {
+  prevBtn.addEventListener("click", handleSlidePrev);
+}
+nextBtn.addEventListener("click", handleSlideNext);
+window.addEventListener("scroll", handleNavbarFontDown);
+window.addEventListener("scroll", handleNavbarFontUp);
